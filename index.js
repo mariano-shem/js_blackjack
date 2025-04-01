869fe9553e1d0b26146576b1ec30c80b9b908aef
sheeemboi <shemmariano56@gmail.com>
2025-04-01 21:12:15 +0800

    game logic completed

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
-startBtn = document.querySelector(".start")
-restartBtn = document.querySelector(".again")
-addBtn = document.querySelector(".add")
-standBtn = document.querySelector(".stand")
-playerDeck = document.querySelector(".deck-player")
-dealerDeck = document.querySelector(".deck-dealer")
-let playerCards = []
-let dealerCards = []
-let firstPlayerCard, secondPlayerCard
-let firstDealerCard, secondDealerCard
-let sumPlayer, sumDealer
+$start = document.querySelector(".start")
+$restart = document.querySelector(".again")
+$add = document.querySelector(".add")
+$stand = document.querySelector(".stand")
+
+$pCards = document.querySelector(".player")
+$dCards = document.querySelector(".dealer")
+
+let $gMsg = document.querySelector(".game-msg")
+let $pTotal = document.querySelector(".t-player")
+let $dTotal = document.querySelector(".t-dealer")
+
+let pCards = []
+let dCards = []
+let pCard1, pCard2
+let dCard1, dCard2
+let pTotal, dTotal
 
 /* Flags */
 let hasBlackjack = false
+let isDefeated = false
+let isDraw = false
 
-function randomizeCards(firstCard, secondCard) {
-  firstCard = Math.floor(Math.random()*10) + 2
-  secondCard = Math.floor(Math.random()*10) + 2
-  return cardArr = [firstCard, secondCard]
+function randomizeCards(card1, card2) {
+  card1 = Math.floor(Math.random()*10) + 2
+  card2 = Math.floor(Math.random()*10) + 2
+  return cardArr = [card1, card2]
 }
 
 function sumOfCards(cardArr) {
   return sum = cardArr.reduce((acc, card) => acc + card)
 }
 
-function preventExceed(sum, firstCard, arr) {
+function preventExceed(sum, card1, arr) {
   if (sum === 22) {
-    firstCard = 1
-    arr[0] = firstCard
+    card1 = 1
+    arr[0] = card1
     sum = sumOfCards(arr)
   }
 
@@ -34,92 +42,157 @@ function preventExceed(sum, firstCard, arr) {
 }
 
 function checkSum(sum) {
+
   if (sum <= 20) {
-    console.log(`Your cards total to ${sum}! Hit another one?`)
+    $gMsg.textContent = `Hit or Stand? Current: ${sum}`
   } else if (sum === 21) {
     hasBlackjack = true
-    console.log(`Blackjack!`)
+    $gMsg.textContent = `Blackjack!`
+    $stand.style.display = "none"
+    $add.style.display = "none"
+
+    $dCards.querySelectorAll("span")[1].textContent = dCards[1]
+    $dTotal.textContent = dTotal
   } else {
-    console.log(`You've exceeded your cards for a total of ${sum}!`)
+    isDefeated = true
+    $stand.style.display = "none"
+    $add.style.display = "none"
+    $gMsg.textContent = `You lost. Total: ${sum}`
+
+    $dCards.querySelectorAll("span")[1].textContent = dCards[1]
+    $dTotal.textContent = dTotal
+  }
+
+  $pTotal.textContent = sum
+}
+
+function dealerBlackJack() {
+  if (dTotal === 21) {
+    isDefeated = true
+    $stand.style.display = "none"
+    $add.style.display = "none"
+    $dTotal.textContent = dTotal
+    $dCards.querySelectorAll("span")[1].textContent = dCards[1]
   }
 }
 
-/* Start Game */
-startBtn.addEventListener("click", startGame = () => {
+function bothBlackJack() {
+  if (dTotal === 21 && pTotal === 21) {
+    isDraw = true
+    $stand.style.display = "none"
+    $add.style.display = "none"
+    $dTotal.textContent = dTotal
+    $dCards.querySelectorAll("span")[1].textContent = dCards[1]
+  }
+}
 
-  restartBtn.style.display = "inline"
-  addBtn.style.display = "inline"
-  standBtn.style.display = "inline"
-
-  // Randomizes first two cards of both player and dealer
-  playerCards = randomizeCards(firstPlayerCard, secondPlayerCard, playerCards)
-  dealerCards = randomizeCards(firstDealerCard, secondDealerCard, dealerCards)
-
-  // Returns sum of first two cards
-  sumPlayer = sumOfCards(playerCards)
-  sumDealer = sumOfCards(dealerCards)
-
-  // Converts 1st card to 1 if both cards are valued at 11
-  sumPlayer = preventExceed(sumPlayer, firstPlayerCard, playerCards)
-  sumDealer = preventExceed(sumDealer, firstDealerCard, dealerCards)
-
-  checkSum(sumPlayer)
-
-  // Displays all player cards on screen
-  for (let card of playerCards) {
-    let thisCard = document.createElement("span")
-    thisCard.textContent = card
-    playerDeck.append(thisCard)
+function checkWin() {
+  if (dTotal < 22) {
+    if (dTotal === pTotal) {
+      isDefeated = false
+      isDraw = true
+      $gMsg.textContent = "Draw!"
+    } else if (dTotal > pTotal) {
+      isDefeated = true
+      isDraw = false
+      $gMsg.textContent = "You lost."
+    } else {
+      isDefeated = false
+      isDraw = false
+      $gMsg.textContent = "You win!"
+    }
+  } else {
+    isDefeated = false
+    $gMsg.textContent = "You win!"
   }
 
+  console.log(`isDraw: ${isDraw}`)
+  console.log(`isDefeated: ${isDefeated}`)
+}
+
+/* Start Game */
+$start.addEventListener("click", startGame = () => {
+
+  $restart.style.display = "inline"
+  $add.style.display = "inline"
+  $stand.style.display = "inline"
+
+  // Randomizes first two cards of both player and dealer
+  pCards = randomizeCards(pCard1, pCard2, pCards)
+  dCards = randomizeCards(dCard1, dCard2, dCards)
+
+  // Returns sum of first two cards
+  pTotal = sumOfCards(pCards)
+  dTotal = sumOfCards(dCards)
+
+  // Converts 1st card to 1 if both cards are valued at 11
+  pTotal = preventExceed(pTotal, pCard1, pCards)
+  dTotal = preventExceed(dTotal, dCard1, dCards)
+
+  checkSum(pTotal)
+
+  $dTotal.textContent = ""
+
+  
+  // Displays all player cards on screen
+  for (let card of pCards) {
+    let $thisCard = document.createElement("span")
+    $thisCard.textContent = card
+    $pCards.append($thisCard)
+    
+  }
+  
   // Displays first dealer card only on screen
-  let dealerShow = document.createElement("span")
-  dealerShow.textContent = dealerCards[0]
-  dealerDeck.append(dealerShow)
-
+  for (let card of dCards) {
+    let $thisCard = document.createElement("span")
+    if (dCards.indexOf(card) === 0) {
+      $thisCard.textContent = card
+    } else {
+      $thisCard.textContent = "?"
+    }
+    $dCards.append($thisCard)
+  }
+  
   // Removes Start button on first game
-  startBtn.style.display = "none"
-  console.log(`Blackjack?: ${hasBlackjack}`)
-  console.log(`Player cards: ${playerCards}`)
-  console.log(`Dealer cards: ${dealerCards}`)
-
+  $start.style.display = "none"
+  
+  bothBlackJack()
+  dealerBlackJack()
+  
+  console.log(`Dealer: ${dTotal}`)
 })
 
-/* Add Card on Deck */
-addBtn.addEventListener("click", () => {
+/* Add Card on Hand */
+$add.addEventListener("click", () => {
 
-  newCard = Math.floor(Math.random()*10) + 2
-  playerCards.push(newCard)
-  sumPlayer = sumOfCards(playerCards)
+  let newCard = Math.floor(Math.random()*10) + 2
+  pCards.push(newCard)
+  pTotal = sumOfCards(pCards)
 
   // Displays added card on screen
-  let thisCard = document.createElement("span")
-  thisCard.textContent = newCard
-  playerDeck.append(thisCard)
-  
-  console.log(`Another hit! Your total is now ${sum}!`)
-  checkSum(sumPlayer)
+  let $thisCard = document.createElement("span")
+  $thisCard.textContent = newCard
+  $pCards.append($thisCard)
 
-  console.log(hasBlackjack)
-  console.log(playerCards)
+  checkSum(pTotal)
 })
 
 /* Remove All Card */
-restartBtn.addEventListener("click", () => {
+$restart.addEventListener("click", () => {
 
   hasBlackjack = false
 
   // Deletes cards inside array
-  playerCards.length = 0
-  dealerCards.length = 0
+  pCards.length = 0
+  dCards.length = 0
 
   // Deletes cards on display
   
-  let allDecks = document.querySelectorAll(".deck")
+  let allHand = document.querySelectorAll(".hand")
 
-  for (let deck of allDecks) {
-    let allCards = deck.querySelectorAll("span")
-    for (let thisCard of allCards) {
+  for (let hand of allHand) {
+    let $allCards = hand.querySelectorAll("span")
+    for (let thisCard of $allCards) {
       thisCard.remove()
     }
   }
@@ -127,8 +200,30 @@ restartBtn.addEventListener("click", () => {
   startGame()
 })
 
-/* Stand Current Cards */
-standBtn.addEventListener("click", () => {
+/* Stand Cards in Hand */
+$stand.addEventListener("click", () => {
+  $dCards.querySelectorAll("span")[1].textContent = dCards[1]
+  $add.style.display = "none"
+  $stand.style.display = "none"
   
+  while (dTotal < 17) {
+    let $thisCard = document.createElement("span")
+    let newCard = Math.floor(Math.random()*10) + 2
+
+    dCards.push(newCard)
+    dTotal = sumOfCards(dCards)
+
+    $thisCard.textContent = newCard
+    $dCards.append($thisCard)
+
+    console.log(dCards)
+    console.log(dTotal)
+  } 
+  
+  $dTotal.textContent = dTotal
+
+  checkWin()
 })
+
+
 
